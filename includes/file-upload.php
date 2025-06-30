8805fb7991c15214e5a28a3a3631de9fd5a05e6d
INB-RTBritos <154307214+INB-RTBritos@users.noreply.github.com>
2025-06-30 20:38:30 +0800

    file upload ugh

---
+++ includes/file-upload.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Upload Files</h2>
+    <form method="POST" enctype="multipart/form-data" action="process-upload.php">
+        <label for="file">Upload File</label>
+        <input type="file" id = "file" name = "file">
+        <button>Upload</button>
+    </form>
+</body>
+</html>
---
+++ includes/process-upload.php
@@ -0,0 +1,84 @@
+<?php 
+
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    // can be page for where the file upload was initiated
+    header("Location: file-upload.php");
+}
+
+if (empty($_FILES)) {
+    // can be page for where the file upload was initiated
+    header("Location: file-upload.php");
+}
+
+if ($_FILES["file"]["error"] !== UPLOAD_ERR_OK) {
+    
+    switch ($_FILES["file"]["error"]) {
+        case UPLOAD_ERR_PARTIAL:
+            // popup trigger nalang, pero tsaka na pag gets ko na js non (temp lang muna to)
+            header("Location: file-upload.php");
+            break;
+        case UPLOAD_ERR_NO_FILE:
+            header("Location: file-upload.php");
+            break;
+        case UPLOAD_ERR_FORM_SIZE:
+            header("Location: file-upload.php");
+            break;
+        case UPLOAD_ERR_INI_SIZE:
+            header("Location: file-upload.php");
+            break;
+        case UPLOAD_ERR_NO_TMP_DIR:
+            header("Location: file-upload.php");
+            break;
+        case UPLOAD_ERR_CANT_WRITE:
+            header("Location: file-upload.php");
+            break;
+        default:
+            exit("Unknown upload error");
+            break;
+    }
+} 
+
+// file size restriction
+if ($_FILES["file"]["size"] > 25600000) {
+    exit ("Files can be no more than 25 MB");
+}
+
+// getting file types
+$finfo = new finfo (FILEINFO_MIME_TYPE);
+// gets the file type by looking at the tmp_name arrays in $_FILES
+$mime_type = $finfo->file($_FILES["file"]["tmp_name"]);
+// array for valid file types accepted
+$mime_types = ["image/gif", "image/png", "image/jpeg"];
+// checks if file type of uploaded file is found in the array
+if ( ! in_array($mime_type, $mime_types)) {
+    exit ("Invalid File Type.");
+}
+
+// moving the uploaded file from the temporary folder to the uploads directory
+$pathinfo = pathinfo($_FILES["file"]["name"]);
+// separates the filename from the extension
+$base = $pathinfo["filename"];
+// checks for filenames that can be similar to directories as filenames
+$base = preg_replace("/[^\w-]/", "_", $base);
+// concatenates the extension of the file uploaded to the extracted filename
+$filename = $base . "." . $pathinfo["extension"];
+// sets the where the uploaded files are stored
+$destination = "/xampp/htdocs/task-manager/assets/uploads/" . "$filename";
+
+// cases where duplicated filenames can be accepted
+$i = 1;
+
+while (file_exists($destination)) {
+
+    $filename = $base . "($i)." .  $pathinfo["extension"];
+    $destination = "/xampp/htdocs/task-manager/assets/uploads/" . $filename;
+
+    $i++;
+}
+
+if ( ! move_uploaded_file($_FILES["file"]["tmp_name"], $destination)) {
+    exit ("File can't be moved.");
+}
+
+echo ("File uploaded succesfully");
+?>
