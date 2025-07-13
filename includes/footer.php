3515cd8124d79fcc141fc510ed1242c5e1dacbfc
Nerr <218392136+nerrner@users.noreply.github.com>
2025-07-13 18:50:35 +0800

    ugh footer

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+        <div class="container">
+            <p>&copy; <?php echo date('Y'); ?> Ahjin No Moto. All rights reserved.</p>
+        </div>
+</footer>
+</body>
+</html>
