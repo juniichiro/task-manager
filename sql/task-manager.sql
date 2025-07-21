9f753a9b3600bd9f0a9a4f3319df30afecfbf22d
INB-Britos <britosranieljohn@gmail.com>
2025-07-21 21:13:42 +0800

    ugh sql file

--- sql/task-manager.sql
+++ sql/task-manager.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 14, 2025 at 03:13 PM
+-- Generation Time: Jul 21, 2025 at 03:01 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -52,20 +52,17 @@ CREATE TABLE `notes` (
   `notes_id` int(255) NOT NULL,
   `notes_title` varchar(1000) NOT NULL,
   `notes_description` mediumtext NOT NULL,
-  `task_id` int(255) NOT NULL
+  `task_id` int(255) NOT NULL,
+  `user_id` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `notes`
 --
 
-INSERT INTO `notes` (`notes_id`, `notes_title`, `notes_description`, `task_id`) VALUES
-(2, 'test', 'test', 1),
-(7, 'test', 'test', 1),
-(8, '2', '2', 2),
-(9, '1', '1', 2),
-(10, 'test', 'test', 2),
-(11, 'testt', 'testt', 2);
+INSERT INTO `notes` (`notes_id`, `notes_title`, `notes_description`, `task_id`, `user_id`) VALUES
+(7, 'test', 'test', 1, 9),
+(13, 'testtest', 'teste', 3, 5);
 
 -- --------------------------------------------------------
 
@@ -75,16 +72,36 @@ INSERT INTO `notes` (`notes_id`, `notes_title`, `notes_description`, `task_id`) 
 
 CREATE TABLE `task` (
   `task_id` int(255) NOT NULL,
-  `task_name` varchar(1000) NOT NULL
+  `task_name` varchar(1000) NOT NULL,
+  `user_id` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `task`
 --
 
-INSERT INTO `task` (`task_id`, `task_name`) VALUES
-(1, 'test'),
-(2, 'teste');
+INSERT INTO `task` (`task_id`, `task_name`, `user_id`) VALUES
+(1, 'test', 9),
+(3, 'testing123', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `upload`
+--
+
+CREATE TABLE `upload` (
+  `file_id` int(255) NOT NULL,
+  `file_name` varchar(1000) NOT NULL,
+  `file_path` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `upload`
+--
+
+INSERT INTO `upload` (`file_id`, `file_name`, `file_path`) VALUES
+(1, 'test(2).png', '../assets/uploads/test(2).png');
 
 --
 -- Indexes for dumped tables
@@ -101,13 +118,21 @@ ALTER TABLE `accounts`
 --
 ALTER TABLE `notes`
   ADD PRIMARY KEY (`notes_id`) USING BTREE,
-  ADD KEY `task_id` (`task_id`);
+  ADD KEY `userf` (`user_id`),
+  ADD KEY `notes_ibfk_1` (`task_id`);
 
 --
 -- Indexes for table `task`
 --
 ALTER TABLE `task`
-  ADD PRIMARY KEY (`task_id`);
+  ADD PRIMARY KEY (`task_id`),
+  ADD KEY `foreign` (`user_id`);
+
+--
+-- Indexes for table `upload`
+--
+ALTER TABLE `upload`
+  ADD PRIMARY KEY (`file_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -117,19 +142,25 @@ ALTER TABLE `task`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `user_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `user_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `notes`
 --
 ALTER TABLE `notes`
-  MODIFY `notes_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `notes_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `task`
 --
 ALTER TABLE `task`
-  MODIFY `task_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `task_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `upload`
+--
+ALTER TABLE `upload`
+  MODIFY `file_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -139,7 +170,14 @@ ALTER TABLE `task`
 -- Constraints for table `notes`
 --
 ALTER TABLE `notes`
-  ADD CONSTRAINT `notes_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`);
+  ADD CONSTRAINT `notes_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `userf` FOREIGN KEY (`user_id`) REFERENCES `accounts` (`user_id`);
+
+--
+-- Constraints for table `task`
+--
+ALTER TABLE `task`
+  ADD CONSTRAINT `foreign` FOREIGN KEY (`user_id`) REFERENCES `accounts` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
