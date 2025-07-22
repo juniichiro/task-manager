5dd803bfc390f8ed1461fed4cb7f42d9ede37f5e
INB-Britos <britosranieljohn@gmail.com>
2025-07-23 00:21:36 +0800

    ugh update

--- pages/task/update.php
+++ pages/task/update.php
@@ -1,7 +1,8 @@
 <?php
 require "../../includes/dbconnection.php";
 $id = $_POST['task_ID'];
-$List = $_POST['itask'];
-$query = $db->query("UPDATE tasklist SET Task_Name = '$List' WHERE list_ID = '$id'");
+$task_name = $_POST['task_name'];
+
+$query = $db->query("UPDATE task SET task_name = '$task_name' WHERE task_id = '$id'");
 header("Location: indexhome.php");
 ?>
