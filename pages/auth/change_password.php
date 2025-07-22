960604f6d71951973b29ed3c5d5dd326d705750c
Nerr <218392136+nerrner@users.noreply.github.com>
2025-07-22 19:32:14 +0800

    ugh berry pie

--- pages/auth/change_password.php
+++ pages/auth/change_password.php
@@ -10,31 +10,33 @@
 ?>  
     <link rel="stylesheet" href="auth.css">
     <div class="wrapper">
-        <!-- LOGIN FORM -->
+        <!-- CHANGE PASSWORD FORM -->
         <span class="icon-close">
             <a href="../front/front.html"><ion-icon name="close-outline"></ion-icon></a>
         </span>
 
-        <form action="login_submit.php" method = "POST" class="login-form" autocomplete="off">
-            <h2>Login</h2>
+        <form action="#" method = "POST" class="change-pass-form" autocomplete="off">
+            <h2>Change Password</h2>
+            
             <div class="input-box">
-                <input type="text" class="input-field" name="email" required>
-                <label for="log-email" class="label">Email</label>
-                <i class='bx bx-envelope icon'></i>
+                <input type="password" class="input-field" name="new-password" required>
+                <label for="log-pass" class="label">New Password</label>
+                <i class='bx bx-lock-alt icon'></i>
             </div>
             <div class="input-box">
-                <input type="password" class="input-field" name="password" required>
-                <label for="log-pass" class="label">Password</label>
+                <input type="password" class="input-field" name="conf-new-password" required>
+                <label for="log-pass" class="label">Confirm Password</label>
                 <i class='bx bx-lock-alt icon'></i>
             </div>
             <div class="form-cols">
                 <div class="col-1"></div>
                 <div class="col-2"> 
-                    <a href="../auth/verify_email.php">Forgot password?</a>
+                    <a href="../auth/login.php">Back to Login</a>
                 </div>
             </div>
+
             <div class="input-box">
-                <button class="btn-submit" id="SignInBtn">Sign In <i class='bx bx-log-in'></i></button>
+                <button class="btn-submit" id="ChangePassBtn">Change Password <i class='bx bx-repost'></i></button>
             </div>
             <div class="switch-form">
                 <span>Don't have an account? <a href="registration.php" onclick="">Register</a></span>
--- pages/auth/verify_email.php
+++ pages/auth/verify_email.php
@@ -10,31 +10,28 @@
 ?>  
     <link rel="stylesheet" href="auth.css">
     <div class="wrapper">
-        <!-- LOGIN FORM -->
+        <!-- VEFIFY EMAIL FORM -->
         <span class="icon-close">
             <a href="../front/front.html"><ion-icon name="close-outline"></ion-icon></a>
         </span>
 
-        <form action="login_submit.php" method = "POST" class="login-form" autocomplete="off">
-            <h2>Login</h2>
+        <form action="#" method = "POST" class="verify-form" autocomplete="off">
+            <h2>Verify Email</h2>
+
             <div class="input-box">
-                <input type="text" class="input-field" name="email" required>
+                <input type="email" class="input-field" name="email" required>
                 <label for="log-email" class="label">Email</label>
                 <i class='bx bx-envelope icon'></i>
-            </div>
-            <div class="input-box">
-                <input type="password" class="input-field" name="password" required>
-                <label for="log-pass" class="label">Password</label>
-                <i class='bx bx-lock-alt icon'></i>
             </div>
             <div class="form-cols">
                 <div class="col-1"></div>
                 <div class="col-2"> 
-                    <a href="../auth/verify_email.php">Forgot password?</a>
+                    <a href="../auth/login.php">Back to Login</a>
                 </div>
             </div>
+
             <div class="input-box">
-                <button class="btn-submit" id="SignInBtn">Sign In <i class='bx bx-log-in'></i></button>
+                <button class="btn-submit" id="VerifyEmailBtn"> Send Verification <i class='bx bx-check-shield'></i></button>
             </div>
             <div class="switch-form">
                 <span>Don't have an account? <a href="registration.php" onclick="">Register</a></span>
